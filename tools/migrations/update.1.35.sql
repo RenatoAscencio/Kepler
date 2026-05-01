de5df28fd9d03acf60cc6ab74c813c12bc41162d
Renato Ascencio <renato_ascencio@hotmail.com>
2026-05-01 17:45:59 -0600

    fix: migrate existing club gift labels

--- tools/migrations/update.1.35.sql
+++ tools/migrations/update.1.35.sql
@@ -1,3 +1,13 @@
 UPDATE `settings`
 SET `value` = '¡Acabas de recibir tu regalo mensual del Club!'
 WHERE `setting` = 'club.gift.present.label';
+
+UPDATE `items` AS i
+INNER JOIN `items_definitions` AS d ON d.`id` = i.`definition_id`
+SET i.`custom_data` = REPLACE(
+    i.`custom_data`,
+    '|You have just received your monthly club gift!|',
+    '|¡Acabas de recibir tu regalo mensual del Club!|'
+)
+WHERE d.`behaviour` LIKE '%present%'
+  AND i.`custom_data` LIKE '%|You have just received your monthly club gift!|%';
