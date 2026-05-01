7f556d30b6219450f865f5d303c137461939b091
Renato Ascencio <renato_ascencio@hotmail.com>
2026-05-01 10:01:20 -0600

    fix: restore native game handshake

--- Kepler-Server/src/main/java/org/alexdev/kepler/server/netty/NettyChannelInitializer.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/server/netty/NettyChannelInitializer.java
@@ -3,15 +3,9 @@ package org.alexdev.kepler.server.netty;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpServerCodec;
-import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.timeout.IdleStateHandler;
 import org.alexdev.kepler.server.netty.codec.NetworkDecoder;
 import org.alexdev.kepler.server.netty.codec.NetworkEncoder;
-import org.alexdev.kepler.server.netty.codec.websocket.WebSocketHandshakeCompleteHandler;
-import org.alexdev.kepler.server.netty.codec.websocket.ProtocolDetector;
-import org.alexdev.kepler.server.netty.codec.websocket.WebSocketBinaryFrameCodec;
 import org.alexdev.kepler.server.netty.connections.ConnectionHandler;
 import org.alexdev.kepler.server.netty.connections.IdleConnectionHandler;
 
@@ -25,20 +19,7 @@ public class NettyChannelInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
         ChannelPipeline pipeline = socketChannel.pipeline();
-        pipeline.addLast("protocolDetector", new ProtocolDetector(
-                this::configureWebSocket,
-                this::configureNative
-        ));
-    }
-
-    private void configureWebSocket(ChannelPipeline pipeline) {
-        pipeline.addLast("httpCodec", new HttpServerCodec());
-        pipeline.addLast("httpAggregator", new HttpObjectAggregator(65536));
-        pipeline.addLast("wsProtocol", new WebSocketServerProtocolHandler("/", null, true, 65536));
-        pipeline.addLast("wsHandshakeComplete", new WebSocketHandshakeCompleteHandler(p -> {
-            p.addLast("wsCodec", new WebSocketBinaryFrameCodec());
-            configureNative(p);
-        }));
+        configureNative(pipeline);
     }
 
     private void configureNative(ChannelPipeline pipeline) {
