94476eb3165a553d01599e94ea7fe4ab903e853e
Renato Ascencio <renato_ascencio@hotmail.com>
2026-05-01 18:12:02 -0600

    fix: sanitize catalog furni asset mappings

---
+++ tools/migrations/update.1.37.sql
@@ -0,0 +1,73 @@
+UPDATE `catalogue_items` SET `definition_id` = 1058 WHERE `definition_id` = 1599;
+UPDATE `items` SET `definition_id` = 1058 WHERE `definition_id` = 1599;
+UPDATE `catalogue_items` SET `definition_id` = 353 WHERE `definition_id` = 1821;
+UPDATE `items` SET `definition_id` = 353 WHERE `definition_id` = 1821;
+UPDATE `catalogue_items` SET `definition_id` = 354 WHERE `definition_id` = 1822;
+UPDATE `items` SET `definition_id` = 354 WHERE `definition_id` = 1822;
+UPDATE `catalogue_items` SET `definition_id` = 313 WHERE `definition_id` = 1881;
+UPDATE `items` SET `definition_id` = 313 WHERE `definition_id` = 1881;
+UPDATE `catalogue_items` SET `definition_id` = 300 WHERE `definition_id` = 1962;
+UPDATE `items` SET `definition_id` = 300 WHERE `definition_id` = 1962;
+UPDATE `catalogue_items` SET `definition_id` = 149 WHERE `definition_id` = 2063;
+UPDATE `items` SET `definition_id` = 149 WHERE `definition_id` = 2063;
+UPDATE `catalogue_items` SET `definition_id` = 174 WHERE `definition_id` = 2068;
+UPDATE `items` SET `definition_id` = 174 WHERE `definition_id` = 2068;
+UPDATE `catalogue_items` SET `definition_id` = 344 WHERE `definition_id` = 2069;
+UPDATE `items` SET `definition_id` = 344 WHERE `definition_id` = 2069;
+UPDATE `catalogue_items` SET `definition_id` = 327 WHERE `definition_id` = 2072;
+UPDATE `items` SET `definition_id` = 327 WHERE `definition_id` = 2072;
+UPDATE `catalogue_items` SET `definition_id` = 260 WHERE `definition_id` = 2076;
+UPDATE `items` SET `definition_id` = 260 WHERE `definition_id` = 2076;
+UPDATE `catalogue_items` SET `definition_id` = 1139 WHERE `definition_id` = 2094;
+UPDATE `items` SET `definition_id` = 1139 WHERE `definition_id` = 2094;
+UPDATE `catalogue_items` SET `definition_id` = 1138 WHERE `definition_id` = 2095;
+UPDATE `items` SET `definition_id` = 1138 WHERE `definition_id` = 2095;
+UPDATE `catalogue_items` SET `definition_id` = 1115 WHERE `definition_id` = 2098;
+UPDATE `items` SET `definition_id` = 1115 WHERE `definition_id` = 2098;
+UPDATE `catalogue_items` SET `definition_id` = 714 WHERE `definition_id` = 2100;
+UPDATE `items` SET `definition_id` = 714 WHERE `definition_id` = 2100;
+UPDATE `catalogue_items` SET `definition_id` = 264 WHERE `definition_id` = 2107;
+UPDATE `items` SET `definition_id` = 264 WHERE `definition_id` = 2107;
+UPDATE `catalogue_items` SET `definition_id` = 280 WHERE `definition_id` = 2108;
+UPDATE `items` SET `definition_id` = 280 WHERE `definition_id` = 2108;
+UPDATE `catalogue_items` SET `definition_id` = 285 WHERE `definition_id` = 2109;
+UPDATE `items` SET `definition_id` = 285 WHERE `definition_id` = 2109;
+UPDATE `catalogue_items` SET `definition_id` = 314 WHERE `definition_id` = 2235;
+UPDATE `items` SET `definition_id` = 314 WHERE `definition_id` = 2235;
+
+UPDATE `items_definitions` SET `sprite` = 'ads_calip_cola*1', `sprite_id` = 11126, `name` = 'Calippo Lima', `description` = 'Of the most refreshing!', `colour` = '#FFFFFF,#FFFFFF,#FFFFFF,#d2ff00,#FFFFFF', `length` = 1, `width` = 1 WHERE `id` = 1443;
+UPDATE `items_definitions` SET `sprite` = 'ads_idol_jukebox*1', `sprite_id` = 1590, `name` = 'American Idol Jukebox', `description` = 'I sound better already!', `colour` = '#FFFFFF,#FFFFFF', `length` = 1, `width` = 1 WHERE `id` = 1481;
+UPDATE `items_definitions` SET `sprite` = 'barrier*3', `sprite_id` = 112, `name` = 'Red Road Barrier', `description` = 'No trespassing, please!', `colour` = '#FFFFFF,#FFFFFF,#CC3232,#CC3232,#FFFFFF', `length` = 1, `width` = 2 WHERE `id` = 1561;
+UPDATE `items_definitions` SET `sprite` = 'diner_bardesk*1', `sprite_id` = 2851, `name` = 'Aquamarine Bar', `description` = 'Pull up a stool.', `colour` = '#FFFFFF,#ABD0D2', `length` = 1, `width` = 1 WHERE `id` = 1641;
+UPDATE `items_definitions` SET `sprite` = 'diner_bardesk_corner*8', `sprite_id` = 2877, `name` = 'Yellow Corner', `description` = 'Now that''s smooth.', `colour` = '#FFFFFF,#FFD837', `length` = 1, `width` = 1 WHERE `id` = 1642;
+UPDATE `items_definitions` SET `sprite` = 'diner_bardesk_gate*2', `sprite_id` = 2843, `name` = 'Pink Gate', `description` = 'Smothered in syrup and butter!', `colour` = '#FFFFFF,#FF99BC,#FFFFFF,#FF99BC,#FFFFFF,#FF99BC', `length` = 1, `width` = 1 WHERE `id` = 1643;
+UPDATE `items_definitions` SET `sprite` = 'diner_cashreg*6', `sprite_id` = 2821, `name` = 'Blue Register', `description` = 'Roll up roll up!', `colour` = '#FFFFFF,#5EAAF8,#FFFFFF', `length` = 1, `width` = 1 WHERE `id` = 1644;
+UPDATE `items_definitions` SET `sprite` = 'diner_chair*5', `sprite_id` = 2865, `name` = 'Beige Stool', `description` = 'Perch in comfort.', `colour` = '#FFFFFF,#F7EBBC', `length` = 1, `width` = 1 WHERE `id` = 1645;
+UPDATE `items_definitions` SET `sprite` = 'diner_gumvendor*5', `sprite_id` = 2831, `name` = 'Beige Gum Machine', `description` = 'A chewy mouthful.', `colour` = '#FFFFFF,#F7EBBC', `length` = 1, `width` = 1 WHERE `id` = 1646;
+UPDATE `items_definitions` SET `sprite` = 'gothic_chair*6', `sprite_id` = 2099, `name` = 'Gothic Chair Blue', `description` = 'The dark side of Habbo', `colour` = '#ffffff,#336666,#ffffff,#336666', `length` = 1, `width` = 1 WHERE `id` = 1731;
+UPDATE `items_definitions` SET `sprite` = 'gothic_sofa*6', `sprite_id` = 2100, `name` = 'Gothic Sofa Blue', `description` = 'The dark side of Habbo', `colour` = '#ffffff,#336666,#ffffff,#ffffff,#336666,#ffffff', `length` = 2, `width` = 1 WHERE `id` = 1732;
+UPDATE `items_definitions` SET `sprite` = 'gothic_stool*6', `sprite_id` = 2101, `name` = 'Gothic Stool Blue', `description` = 'The dark side of Habbo', `colour` = '#ffffff,#336666,#ffffff', `length` = 1, `width` = 1 WHERE `id` = 1733;
+UPDATE `items_definitions` SET `sprite` = 'greektrophy*2', `sprite_id` = 1383, `name` = 'Greek trophy', `description` = 'Shiny silver', `colour` = '#FFFFFF,#FFFFFF,#DDDDDD', `length` = 1, `width` = 1 WHERE `id` = 1741;
+UPDATE `items_definitions` SET `sprite` = 'noob_chair*4', `sprite_id` = 1168, `name` = 'My first Habbo chair', `description` = 'Lightweight, practical, with light blue stripes', `colour` = '#FFFFFF,#80B1BC,#DAF4FA,#FFFFFF,#DAF4FA,#80B1BC', `length` = 1, `width` = 1 WHERE `id` = 1889;
+UPDATE `items_definitions` SET `sprite` = 'noob_lamp*2', `sprite_id` = 1172, `name` = 'My first Habbo lamp', `description` = 'Get the light right where you want it (dark blue)', `colour` = '#8D92AB,#FFFFFF,#FFFFFF', `length` = 1, `width` = 1 WHERE `id` = 1891;
+UPDATE `items_definitions` SET `sprite` = 'noob_rug*3', `sprite_id` = 1179, `name` = 'My first Habbo rug', `description` = 'Nice and neat sisal rug with aubergine edging', `colour` = '#985A47,#D9AD90', `length` = 2, `width` = 3 WHERE `id` = 1894;
+UPDATE `items_definitions` SET `sprite` = 'noob_stool*3', `sprite_id` = 1185, `name` = 'My first Habbo stool', `description` = 'Unfold me and take the weight off (aubergine)', `colour` = '#FFFFFF,#985A47', `length` = 1, `width` = 1 WHERE `id` = 1896;
+UPDATE `items_definitions` SET `sprite` = 'noob_table*3', `sprite_id` = 1191, `name` = 'My first Habbo table', `description` = 'Lightweight, practical and aubergine', `colour` = '#FFFFFF,#D9AD90,#985A47', `length` = 2, `width` = 2 WHERE `id` = 1897;
+UPDATE `items_definitions` SET `sprite` = 'one_way_door*4', `sprite_id` = 9803, `name` = 'Beige One Way Gate', `description` = 'One at a time!', `colour` = '#F7EBBC,#FFFFFF,#F7EBBC,#FFFFFF,#FFFFFF', `length` = 1, `width` = 1 WHERE `id` = 1901;
+UPDATE `items_definitions` SET `sprite` = 'pillar*3', `sprite_id` = 1083, `name` = 'blue pillar', `description` = 'Ancient and stately', `colour` = '#ffffff,#BDCDEA,#BDCDEA', `length` = 1, `width` = 1 WHERE `id` = 1961;
+UPDATE `items_definitions` SET `sprite` = 'plant_valentinerose*2', `sprite_id` = 680, `name` = 'White Valentine''s Rose', `description` = 'For the purest love', `colour` = '#FFFFFF,#FFFFFF,#FFFFFF,#FFFFFF', `length` = 1, `width` = 1 WHERE `id` = 1967;
+UPDATE `items_definitions` SET `sprite` = 'sleepingbag*5', `sprite_id` = 9903, `name` = 'Graphite Sleeping Bag', `description` = 'Ultimate coziness', `colour` = '#9f9f9f,#ffffff,#9f9f9f,#ffffff,#9f9f9f,#ffffff', `length` = 1, `width` = 3 WHERE `id` = 2124;
+UPDATE `items_definitions` SET `sprite` = 'summer_chair*7', `sprite_id` = 1255, `name` = 'Green Deck Chair', `description` = 'Reserved!', `colour` = '#FFFFFF,#FFFFFF,#FFFFFF,#FFFFFF,#92D13D', `length` = 1, `width` = 1 WHERE `id` = 2135;
+UPDATE `items_definitions` SET `sprite` = 'summer_grill*3', `sprite_id` = 1260, `name` = 'Green Barbeque Grill', `description` = 'Plenty of steak on that barbie', `colour` = '#FFFFFF,#FFFFFF,#FFFFFF,#FFFFFF,#19C819,#19C819,#19C819,#19C819', `length` = 2, `width` = 1 WHERE `id` = 2136;
+UPDATE `items_definitions` SET `sprite` = 'summer_pool*4', `sprite_id` = 1113, `name` = 'Yellow Summer Pool', `description` = 'Fancy a dip?', `colour` = '#FFD837,#FFD837,#FFD837,#FFD837,#FFFFFF,#FFFFFF,#FFFFFF,#FFFFFF,#FFD837', `length` = 2, `width` = 2 WHERE `id` = 2138;
+UPDATE `items_definitions` SET `sprite` = 'traffic_light*3', `sprite_id` = 115, `name` = 'Purple Traffic Light', `description` = 'Chill and wait your turn!', `colour` = '#FFFFFF,#E0337A,#FFFFFF', `length` = 1, `width` = 1 WHERE `id` = 2181;
+UPDATE `items_definitions` SET `sprite` = 'val_teddy*3', `sprite_id` = 1242, `name` = 'Green Share Bear', `description` = 'The green bear of friendship', `colour` = '#7CB135,#FFFFFF,#7CB135,#7CB135,#FFFFFF', `length` = 1, `width` = 1 WHERE `id` = 2213;
+
+UPDATE `catalogue_items`
+SET `is_hidden` = 1
+WHERE `id` IN (
+    215, 1351, 1398, 1400, 1407, 1463, 1464, 1465, 1466, 1495,
+    1500, 1501, 1502, 1540, 1596, 1657, 1669, 1670, 1694, 1778,
+    1821, 1825, 1827, 1830, 1834, 1837, 1838, 1849, 1893, 1894,
+    1895, 2031, 2032, 2053, 2063, 2118, 2119, 2120, 2151, 2195
+);
